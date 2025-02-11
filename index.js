9fabf2652665735e095fbce572e393d4a6028eb8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-11 07:14:59 -0500

    Add w key event listener, which makes player jump up

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ window.addEventListener('keydown', (event) => {
             keys.a.pressed = true
             lastKey = 'a'
             break
+        case 'w':
+            player.velocity.y -= 10
+            break
 
     }
     console.log(event.key)
@@ -106,6 +109,9 @@ window.addEventListener('keyup', (event) => {
             break
         case 'a':
             keys.a.pressed = false
+            break
+        case 'w':
+            keys.w.pressed = false
             break
     }
     console.log(event.key)
