7e46db4cd55c80e4dd53c6e31444ef55720a231a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-12 05:24:04 -0500

    Add a script tag to call on classes.js; render the background image using the Sprite class

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -8,9 +9,15 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
 
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -24,7 +31,7 @@ const player = new Sprite({
     }
 });
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -100,6 +107,7 @@ function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update();
     enemy.update();
 
