4c1d3b7c58e8c3ca784cc1f1dfa222c169825dbf
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-12 09:55:15 -0500

    Chage the gravity so that the players stand on the floor of the background instead of at the bottom on the <canvas> element

--- js/classes.js
+++ js/classes.js
@@ -62,7 +62,7 @@ class Fighter {
         this.position.y += this.velocity.y;
 
         // prevents from falling below canvas
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+        if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
             this.velocity.y = 0
         } else this.velocity.y += gravity;
     }
