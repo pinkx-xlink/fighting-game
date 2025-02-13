d5afbeb1cdd72d5c51afa7b701b166995e76f967
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-13 07:45:06 -0500

    Add condition to resolve issue of player 1's health decreasing when in contact with enemy's attackBox even when enemy wasn't attacking

--- index.js
+++ index.js
@@ -123,10 +123,10 @@ const enemy = new Fighter({
     },
     attackBox: {
         offset: {
-            x: 0,
-            y: 0
+            x: -170,
+            y: 50
         },
-        width: 100,
+        width: 170,
         height: 50
       }
 })
@@ -227,6 +227,10 @@ function animate() {
     console.log('hit player attackbox')
     player.health -= 20;
     document.querySelector('#playerHealth').style.width = player.health + '%'
+  }
+
+  if (enemy.isAttacking && enemy.framesCurrent === 4 ) {
+    enemy.isAttacking = false
   }
     
   // end game based on health
