9404f8edeee3976a630f79b2ab58cf6df6786550
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-13 21:24:48 -0500

    Add conditions to prevent a player's sprite from moving after they die

--- index.js
+++ index.js
@@ -262,39 +262,46 @@ function animate() {
 }
 animate()
 
-window.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'd':
-      keys.d.pressed = true
-      player.lastKey = 'd'
-      break
-    case 'a':
-      keys.a.pressed = true
-      player.lastKey = 'a'
-      break
-    case 'w':
-      player.velocity.y -= 20
-      break
-    case ' ':
-      player.attack()
-      break
-
-    case 'ArrowRight':
-      keys.ArrowRight.pressed = true
-      enemy.lastKey = 'ArrowRight'
-      break
-    case 'ArrowLeft':
-      keys.ArrowLeft.pressed = true
-      enemy.lastKey = 'ArrowLeft'
-      break
-    case 'ArrowUp':
-      enemy.velocity.y -= 20
-      break
-    case 'ArrowDown':
-      enemy.isAttacking = true
-      enemy.attack()
-      break
+window.addEventListener('keydown', (event) => {4
+  if (!player.dead) {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = true
+        player.lastKey = 'd'
+        break
+      case 'a':
+        keys.a.pressed = true
+        player.lastKey = 'a'
+        break
+      case 'w':
+        player.velocity.y -= 20
+        break
+      case ' ':
+        player.attack()
+        break
+    }
   }
+  
+  if (!enemy.dead) {
+    switch(event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+        enemy.lastKey = 'ArrowRight'
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+        enemy.lastKey = 'ArrowLeft'
+        break
+      case 'ArrowUp':
+        enemy.velocity.y -= 20
+        break
+      case 'ArrowDown':
+        enemy.isAttacking = true
+        enemy.attack()
+        break
+    }
+  }
+  
 })
 window.addEventListener('keyup', (event) => {
   switch (event.key) {
